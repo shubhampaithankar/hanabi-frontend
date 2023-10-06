4f448f679b8846aab7e1d94e9035ac863f262496
shubhampaithankar <shubham.paithankar@krayo.io>
2023-10-06 14:39:29 +0530

    Added error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes/Routes';
+import { ErrorBoundary } from 'react-error-boundary';
 
 const App = () => {
   return (
     <>
-      <Suspense fallback={<>Loading...</>}>   
-        <main className="container-fluid" style={{ height: '100vh' }}>
-          <BrowserRouter>
-            <Routes />
-          </BrowserRouter>
-        </main>
-      </Suspense>
+      <ErrorBoundary fallback={<>Something went wrong, please try again later. <a href='/home'>Home</a></>}>
+        <Suspense fallback={<>Loading...</>}>   
+          <main className="container-fluid" style={{ height: '100vh' }}>
+            <BrowserRouter>
+              <Routes />
+            </BrowserRouter>
+          </main>
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 };
